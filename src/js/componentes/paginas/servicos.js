02db74b314d35123ca3a2872f62b147b73f699d1
Matheusssantos1706 <matheussilva1706@gmai.com>
2026-05-30 11:04:39 -0300

    feat: serviços estaticos

--- src/js/componentes/paginas/servicos.js
+++ src/js/componentes/paginas/servicos.js
@@ -1,4 +1,64 @@
 const servicos = `<h1> Esta é a página servicos </h1>
-<p>Agende seu servico</p>`
+    <div class="bem-grid-auto">
+        <div class="bem-card">
+            <img class="bem-card__image" src="image.jpg" alt="Image description">
+            <div class="bem-card__body">
+                <h3 class="bem-card__title">Title</h3>
+                <p>Description</p>
+            </div>
+        </div>
+        <div class="bem-card">
+            <img class="bem-card__image" src="image.jpg" alt="Image description">
+            <div class="bem-card__body">
+                <h3 class="bem-card__title">Title</h3>
+                <p>Description</p>
+            </div>
+        </div>
+        <div class="bem-card">
+            <img class="bem-card__image" src="image.jpg" alt="Image description">
+            <div class="bem-card__body">
+                <h3 class="bem-card__title">Title</h3>
+                <p>Description</p>
+            </div>
+        </div>
+        <div class="bem-card">
+            <img class="bem-card__image" src="image.jpg" alt="Image description">
+            <div class="bem-card__body">
+                <h3 class="bem-card__title">Title</h3>
+                <p>Description</p>
+            </div>
+        </div>
+        <div class="bem-card">
+            <img class="bem-card__image" src="image.jpg" alt="Image description">
+            <div class="bem-card__body">
+                <h3 class="bem-card__title">Title</h3>
+                <p>Description</p>
+            </div>
+        </div>
+        <div class="bem-card">
+            <img class="bem-card__image" src="image.jpg" alt="Image description">
+            <div class="bem-card__body">
+                <h3 class="bem-card__title">Title</h3>
+                <p>Description</p>
+            </div>
+        </div>
+        <div class="bem-card">
+            <img class="bem-card__image" src="image.jpg" alt="Image description">
+            <div class="bem-card__body">
+                <h3 class="bem-card__title">Title</h3>
+                <p>Description</p>
+            </div>
+        </div>
+        <div class="bem-card">
+            <img class="bem-card__image" src="image.jpg" alt="Image description">
+            <div class="bem-card__body">
+                <h3 class="bem-card__title">Title</h3>
+                <p>Description</p>
+            </div>
+        </div>
+        
+    </div>
+
+`
  
-export default servicos
+export default servicos;
