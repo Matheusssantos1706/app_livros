7a9896025d074c60b081460c69cd7e4bb3adb22a
Matheusssantos1706 <matheussilva1706@gmai.com>
2026-05-30 11:58:38 -0300

    Criação da pagina de serviços

--- src/js/componentes/paginas/servicos.js
+++ src/js/componentes/paginas/servicos.js
@@ -1,64 +1,46 @@
-const servicos = `<h1> Esta é a página servicos </h1>
-    <div class="bem-grid-auto">
+let servicos = "";
+const detalhes = [
+    {
+        titulo: 'Toma caneta Kaio Jorge',
+        descricao: 'xxxxxxxx xxxxxxx xxxxxx',
+        imagem: 'src/img/01.webp',
+    },
+    {
+        titulo: 'Menino ney',
+        descricao: 'xxxxxxxx xxxxxxx xxxxxx',
+        imagem: 'src/img/02.webp',
+    },
+    {
+        titulo: 'Garoto Estevão',
+        descricao: 'xxxxxxxx xxxxxxx xxxxxx',
+        imagem: 'src/img/03.webp',
+    },
+    {
+        titulo: 'Mascote da Copa 2026',
+        descricao: 'xxxxxxxx xxxxxxx xxxxxx',
+        imagem: 'src/img/04.webp',
+    },
+    {
+        titulo: ' Fase de Grupos',
+        descricao: 'xxxxxxxx xxxxxxx xxxxxx',
+        imagem: 'src/img/05.jpg',
+    }    
+    
+]
+servicos +=`<div class="bem-grid-auto">`
+for(let i=0; i< detalhes.length; i++){
+servicos += `
+    
         <div class="bem-card">
-            <img class="bem-card__image" src="image.jpg" alt="Image description">
+            <img class="bem-card__image" src="${detalhes[i].imagem}" alt="Image description">
             <div class="bem-card__body">
-                <h3 class="bem-card__title">Title</h3>
-                <p>Description</p>
+                <h3 class="bem-card__title">${detalhes[i].titulo}</h3>
+                <p>${detalhes[i].descricao}</p>
             </div>
-        </div>
-        <div class="bem-card">
-            <img class="bem-card__image" src="image.jpg" alt="Image description">
-            <div class="bem-card__body">
-                <h3 class="bem-card__title">Title</h3>
-                <p>Description</p>
-            </div>
-        </div>
-        <div class="bem-card">
-            <img class="bem-card__image" src="image.jpg" alt="Image description">
-            <div class="bem-card__body">
-                <h3 class="bem-card__title">Title</h3>
-                <p>Description</p>
-            </div>
-        </div>
-        <div class="bem-card">
-            <img class="bem-card__image" src="image.jpg" alt="Image description">
-            <div class="bem-card__body">
-                <h3 class="bem-card__title">Title</h3>
-                <p>Description</p>
-            </div>
-        </div>
-        <div class="bem-card">
-            <img class="bem-card__image" src="image.jpg" alt="Image description">
-            <div class="bem-card__body">
-                <h3 class="bem-card__title">Title</h3>
-                <p>Description</p>
-            </div>
-        </div>
-        <div class="bem-card">
-            <img class="bem-card__image" src="image.jpg" alt="Image description">
-            <div class="bem-card__body">
-                <h3 class="bem-card__title">Title</h3>
-                <p>Description</p>
-            </div>
-        </div>
-        <div class="bem-card">
-            <img class="bem-card__image" src="image.jpg" alt="Image description">
-            <div class="bem-card__body">
-                <h3 class="bem-card__title">Title</h3>
-                <p>Description</p>
-            </div>
-        </div>
-        <div class="bem-card">
-            <img class="bem-card__image" src="image.jpg" alt="Image description">
-            <div class="bem-card__body">
-                <h3 class="bem-card__title">Title</h3>
-                <p>Description</p>
-            </div>
-        </div>
-        
+        </div>        
     </div>
 
 `
- 
+}
+servicos +=`<div class="bem-grid-auto">`
 export default servicos;
