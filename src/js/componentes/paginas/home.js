5dd0fb51c992487384d83a220e9dc7fea5c257a6
Matheusssantos1706 <matheussilva1706@gmai.com>
2026-06-13 10:45:51 -0300

    feat: teste entendendo o array

--- src/js/componentes/paginas/home.js
+++ src/js/componentes/paginas/home.js
@@ -1,4 +1,19 @@
 const home = `<h1> Esta é a pagina inicial </h1>
-<p>Bem Vindo ao meu site</p>`
- 
+<form action="" id="formulario-fila">
+        <label for="idade">Idade:</label>
+        <input
+            type="number"
+            name="idade"
+            id="idade"
+            placeholder="Digite a sua idade"
+            aria-label="Campo para digitar a Idade"
+            
+        >
+        <button id="enviar">Emitir Senha</button>
+    </form>
+    <ul id="lista-de-pessoas">
+        
+    </ul>
+ `
+
 export default home;
